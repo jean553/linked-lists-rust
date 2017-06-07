90d41c77d4fced26fad7b6fba03c7459e65490de
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2017-06-07 14:00:56 +0200

    add documentation about replace

--- linked-lists-rust/src/lib.rs
+++ linked-lists-rust/src/lib.rs
@@ -31,6 +31,11 @@ mod ll {
 
             let node = Box::new(Node {
                 data: data,
+                /* next = self.tail cannot be written because enumerations 
+                   are moved by default; the value of self.tail cannot be moved
+                   as it would let self in an incomplete state after movement;
+                   replace() replaces the value at a mutable location with the new value,
+                   returns the old one, so no moves occure */
                 next: mem::replace(&mut self.tail, Link::Nothing)
             });
 
